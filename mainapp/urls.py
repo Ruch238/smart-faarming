3ac9f19c6d61e27d83d5e73558f603d1369602ab
Rucha <rupatil238@gmail.com>
2023-03-31 10:50:56 +0530

    Deploying to Railway - mainapp/urls.py changes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('subapp.urls')),
+    path('subapp/', include('subapp.urls')),
     path('admin/', admin.site.urls),
-
 ]
