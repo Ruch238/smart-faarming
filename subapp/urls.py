fe6511d6361250b8579434df89fc3c670d4b3950
Rucha <rupatil238@gmail.com>
2023-04-04 10:58:06 +0530

    urls.py changed + static

--- subapp/urls.py
+++ subapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -25,4 +28,4 @@ urlpatterns = [
     path("login", views.login_request, name="login"),
     path("logout", views.logout_request, name="logout"),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
